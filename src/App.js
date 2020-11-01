54e6d7478a52da2c0ebce5cfb05c75162c82dde7
Chance Stribling <47017536+chance-stribling@users.noreply.github.com>
2020-11-01 14:43:20 -0600

    added more text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={stormbw} className="App-logo" alt="Storm" />
+        
         <h1>
         Meet Storm
         </h1>
+        
         <p>
-        I am the mother of two sons, Michael and William, both proud EBR students (Go Belaire Bengals and Sherwood Falcons!) 
+        I am the mother of two sons, Michael and William, both proud EBR students
+        </p>
+        
+        <h6>
+        Go Belaire Bengals and Sherwood Falcons!
+        </h6>
+        
+        <p>
         I have been honored to serve and teach the children of East Baton Rouge Parish Schools for the last 14 years. 
         During that time I have been an active parent, teacher, and union member whether it 
         be as a special needs advocate, fighting for my students, 
         fighting for the safety of both students and teachers, or participating at board meetings.
+        </p>
+        
+        <p>
+        I hold a Bachelors degree in English and a Bachelors in Afican American Studies from Purdue University and 
+        obtained Alternate Certification in Middle School English from TeachBatonRouge.  
         </p>
         <a
           className="App-link"
